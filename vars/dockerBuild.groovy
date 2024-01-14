11d3a104a4565b89b02c547b32e777b7714b6615
Aqdam Rahman <82409296+aqdam11@users.noreply.github.com>
2024-01-14 19:36:21 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,10 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=239d1af2acmshee08c404c7d2a43p13d6cajsn20bd2ae77c2f -t ${dockerimage} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=239d1af2acmshee08c404c7d2a43p13d6cajsn20bd2ae77c2f -t ${imageName} ."
      // Tag the Docker image
-    sh "docker tag ${dockerimage} ${aqdamrahman}/${dockerimage}:latest"
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
     withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
-        sh "docker push ${aqdamrahman}/${dockerimage}:latest"
+        sh "docker push ${dockerHubUsername}/${imageName}:latest"
     }
 }
